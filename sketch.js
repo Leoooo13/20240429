9c07e3867a72751e619ae21b1be455edf550ae1f
Leoooo13 <150640324+Leoooo13@users.noreply.github.com>
2024-04-29 09:53:50 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO)
-  capture.size(320,240)
+  capture.size(640,480)
 }
 
 function draw() {
   background(220);
-  image(capture,mouseX,mouseY)
+  push()
+  translate(width/2-320,height/2-200)
+  image(capture,0,0)
+  pop()
 }
