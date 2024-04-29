4883ec987fdc59578631e3ae6bc202e205c1ef4c
Leoooo13 <150640324+Leoooo13@users.noreply.github.com>
2024-04-29 11:49:56 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,11 +15,15 @@ function setup() {
   radioElement.postition(width/2-300,20)
   radioElement.option("方塊")
   radioElement.option("圓圈")
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
   radioElement.style("color","#fff")
+  radioElement.style("font-size","30px")
 }
 
 function draw() {
-  background(220);
+  background(255);
   push()
   span = 5+map(mouseX,0,width,0,20)
   noStroke()
@@ -29,13 +33,39 @@ function draw() {
     for(var y=0;y<captureGraphics.height ; y=y+span){
       var pixel = captureGraphics.get(x,y)
       fill(pixel)
-      if(radioElement.value()=="方塊"){
+      if(radioElement.value()=="方塊"|| radioElement.value()==""){
       rect(x,y,span)
     }
       if(radioElement.value()=="圓圈"){
       ellipse(x,y,span)
     }
+    if(radioElement.value()=="亮度"){
+      bk =(pixel[0]+pixel[1]+pixel[2])/3
+      fill(bk)
+      ellipse(x,y,span*map(bk,255,255,0,1))
+    }
+    if(radioElement.value()=="紅底"){
+      colorMode(HSB)
+      fill(pixel[0],80,80)
+      push()
+      translate(x,y)
+      rotate(pixel[0]/100)
+      rectMode(CENTER)
+      rect(x,y,span*0.6)
+      fill(0)
+      ellipse(0,0,10)
+      pop()
+    }
+    if(radioElement.value()=="文字"){
+    const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+    let txt = "一二三四五田雷電龕龘"
+    bk =(pixel[0]+pixel[1]+pixel[2])/3
+    let bkId = int(map(bk, 0, 255, 9, 0))
+    textSize(span)
+    textStyle(BOLD)
+    text(txt[bkId],x,y)
+   }
   }
-}
+ }
   pop()
 }
